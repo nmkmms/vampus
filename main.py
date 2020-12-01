f07505a648cd8ce42229030d7a45c55da8b76230
nmkmms <illyakoshmak@gmail.com>
2020-12-01 15:47:33 +0200

    Added fancy cooments:)

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ class Agent:
         self.logic_map = {(x, y): [False, False, False] for x in range(4) for y in range(4)}
         self.route_queue = []
         self.was_here = {(3, 0)}
-        self.know_vampus_position = False
+        self.know_vampus_position, self.know_gold_position = False, False
 
     def get_locations(self):
+        """Get all neighbour cells."""
         locations = []
         if self.x > 0:
             locations.append((self.x - 1, self.y))
@@ -25,39 +26,44 @@ class Agent:
         return {location: [False, False, False] for location in locations}
 
     def perceive(self):
+        """Perceive from nearby cells (feel vampus Stench, Breeze from hole or see Glitter from gold)."""
         locations = self.get_locations()
         hole, gold, vampus = False, False, False
+
+        # Get info from nearby cells
         for location in locations:
             if location not in self.was_here:
-                # curr = self.logic_map[location]
                 feel = self.world[location[0]][location[1]]
-                # new = [curr[num] or feel[num] for num in range(3)]
-                # if new[0]:
-                #     print(f'There is probably a hole in {location}')
-                # if new[2]:
-                #     print(f'There is probably a vampus in {location}')
-                # self.logic_map[location] = new
+
                 if feel[0]: hole = True
-                if feel[1]: gold = True
+                if feel[1] and not self.know_gold_position: gold = True
                 if feel[2] and not self.know_vampus_position: vampus = True
 
+        # Compare new info with knowledge base (KB) and print out thoughts
         for location in locations:
             if location not in self.was_here:
                 if hole:
                     self.logic_map[location][0] = True
                     print(f'There is probably a hole in {location}')
                 if gold:
-                    self.logic_map[location][1] = True
-                    print(f'There is probably a gold in {location}')
+                    if self.logic_map[location][1]:
+                        self.know_gold_position = True
+                        print(f'There is definitely gold in {location}')
+                        self.clear_map(location, 1)
+                    else:
+                        self.logic_map[location][1] = True
+                        print(f'There is probably a gold in {location}')
                 if vampus:
                     if self.logic_map[location][2]:
                         self.know_vampus_position = True
                         print(f'Vampus is definately at {location}')
+                        self.clear_map(location, 2)
                     else:
                         self.logic_map[location][2] = True
                         print(f'There is probably a vampus in {location}')
 
     def clear_map(self, location, item):
+        """Clear logic map if vampus or gold found."""
         for i in range(4):
             for j in range(4):
                 if (i, j) == location:
@@ -66,6 +72,12 @@ class Agent:
                     self.logic_map[(i, j)][item] = 0
 
     def move(self):
+        """Make step."""
+        # Check if agent didn't fall in hole in cell he thought hole is
+        if self.logic_map[(self.x, self.y)][0]:
+            self.logic_map[(self.x, self.y)][0] = False
+            print(f'There is no hole in {(self.x, self.y)}')
+
         locations = self.get_locations()
         for location in locations:
             if location not in self.was_here:
@@ -73,10 +85,12 @@ class Agent:
 
         self.perceive()
 
+        # Sort route queue according to gold, hole and vampus positions in KB
         self.route_queue.sort(key=lambda x: (-self.logic_map[(x[0], x[1])][1],
                                              self.logic_map[(x[0], x[1])][0],
                                              self.logic_map[(x[0], x[1])][2]), reverse=True)
 
+        # Get best step and make it
         step = self.route_queue.pop()
         self.world[self.x][self.y][3] = False
         self.x, self.y = step
@@ -84,7 +98,8 @@ class Agent:
         self.was_here.add((self.x, self.y))
 
 
-class App(tk.Tk):  # build the grid
+# Build gui app
+class App(tk.Tk):
     def __init__(self, world, agent, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
@@ -102,7 +117,9 @@ class App(tk.Tk):  # build the grid
         self.redraw(1000)
 
     def redraw(self, delay):
+        # Parameters to check for results
         fall, killed, won = False, False, False
+        # Redraw world
         for column in range(self.columns):
             for row in range(self.rows):
                 x1 = column * self.cellwidth
@@ -119,6 +136,7 @@ class App(tk.Tk):  # build the grid
                 if self.world[row][column][3]:
                     self.rect[row, column] = self.canvas.create_oval(x1 + 20, y1 + 20, x2 - 20, y2 - 20, fill='red')
 
+        # Check if agent in hole, vampus or gold cells
         for column in range(self.columns):
             for row in range(self.rows):
                 if self.world[row][column][0] and self.world[row][column][3]:
@@ -128,6 +146,7 @@ class App(tk.Tk):  # build the grid
                 if self.world[row][column][1] and self.world[row][column][3]:
                     won = True
 
+        # Get result or keep moving
         if fall:
             print('agent fell in hole!')
         elif killed:
@@ -140,7 +159,9 @@ class App(tk.Tk):  # build the grid
 
 
 def generate_world():
-    # [hole, gold, vampus, agent]
+    """Generate world 4x4 with probability 0.2 of hole in cell, 1 vampus and 1 gold.
+     Every cell is array in form [hole, gold, vampus, agent], where every value is bool.
+     """
     world = []
     for row in range(4):
         r = []
@@ -151,10 +172,12 @@ def generate_world():
                 r.append([False, False, False, False])
         world.append(r)
 
+    # Place gold in any cell except of agent start position
     gold_x, gold_y = 3, 0
     while gold_x == 3 and gold_y == 0:
         gold_x, gold_y = random.randint(0, 3), random.randint(0, 3)
 
+    # Place vampus in any cell except of agent start position and gold position
     vamp_x, vamp_y = 3, 0
     while vamp_x == 3 and vamp_y == 0 or (vamp_x, vamp_y) == (gold_x, gold_y):
         vamp_x, vamp_y = random.randint(0, 3), random.randint(0, 3)
